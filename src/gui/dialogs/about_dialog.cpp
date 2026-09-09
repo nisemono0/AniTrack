c43c4643c6525c8318fcc59f4357b412cdcc6139
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-09-09 22:07:49 +0300

    feat: show utf8proc version in about dialog

--- src/gui/dialogs/about_dialog.cpp
+++ src/gui/dialogs/about_dialog.cpp
@@ -24,7 +24,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
             "tabler-icons: %3\n"
 #ifdef ENABLE_WIP_FEATURES
             "anitomy: %4\n"
-            "anime-relations: %5"
+            "anime-relations: %5\n"
+            "utf8proc: %6"
 #endif // ENABLE_WIP_FEATURES
         ).arg(
             this->ui_->labelAppNameVer->text(),
@@ -33,7 +34,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
 #ifdef ENABLE_WIP_FEATURES
             ,
             this->ui_->labelAnitomyVer->text(),
-            this->ui_->labelAnimeRelationsVer->text()
+            this->ui_->labelAnimeRelationsVer->text(),
+            this->ui_->labelUtf8procVer->text()
 #endif // ENABLE_WIP_FEATURES
         );
 
@@ -68,11 +70,14 @@ void AboutDialog::setupDialog() {
 #ifdef ENABLE_WIP_FEATURES
     this->ui_->labelAnitomyVer->setText(ANITOMY_GIT_COMMIT);
     this->ui_->labelAnimeRelationsVer->setText(ANIME_RELATIONS_GIT_COMMIT);
+    this->ui_->labelUtf8procVer->setText(UTF8PROC_GIT_COMMIT);
 #else
     this->ui_->labelAnitomy->hide();
     this->ui_->labelAnitomyVer->hide();
     this->ui_->labelAnimeRelations->hide();
     this->ui_->labelAnimeRelationsVer->hide();
+    this->ui_->labelUtf8proc->hide();
+    this->ui_->labelUtf8procVer->hide();
 #endif // ENABLE_WIP_FEATURES
 
 }
