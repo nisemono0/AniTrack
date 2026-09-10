e91e3149e184e00bd06b944d4fcb2f3c5a9fe4d0
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-09-11 01:00:57 +0300

    feat: add string similarity

---
+++ src/base/recognition/string_similarity.cpp
@@ -0,0 +1,43 @@
+#include "base/recognition/string_similarity.hpp"
+
+#include <QList>
+
+
+double StringSimilarity::levenshteinSimilarity(const QString &left, const QString &right) {
+    if (left == right) {
+        return 1.0;
+    }
+
+    int left_len = left.size();
+    int right_len = right.size();
+
+    QList<int> prev_row(right_len + 1, 0);
+    QList<int> curr_row(right_len + 1, 0);
+
+    for (int j = 0; j <= right_len; j++) {
+        prev_row[j] = j;
+    }
+
+    for (int i = 1; i <= left_len; i++) {
+        curr_row[0] = i;
+
+        for (int j = 1; j <= right_len; j++) {
+            if (left[i - 1] == right[j - 1]) {
+                curr_row[j] = prev_row[j - 1];
+            }
+            else {
+                curr_row[j] = 1 + qMin(
+                    curr_row[j - 1],
+                    qMin(prev_row[j], prev_row[j - 1])
+                );
+            }
+        }
+
+        prev_row.swap(curr_row);
+    }
+
+    double distance = prev_row.at(right_len);
+
+    return 1.0 - (distance / qMax(left.size(), right.size()));
+}
+
---
+++ src/base/recognition/string_similarity.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <QString>
+
+namespace StringSimilarity {
+double levenshteinSimilarity(const QString &left, const QString &right);
+}
+
