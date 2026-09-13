7b7ee1accb8bcae8c5285c0c1288ba0e598b6cc3
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-09-13 20:36:15 +0300

    feat: add quiet search and search by id
    * quiet search doesn't spawn any progress bars
    * search by id searches anilist using a list of ids

--- src/base/search/search_manager.cpp
+++ src/base/search/search_manager.cpp
@@ -7,20 +7,18 @@ SearchManager::SearchManager(
 ) : QObject(parent),
     anilist_api_(anilist_api) {
 
-    connect(this->anilist_api_, &AnilistApi::searchAnimeFinished, this, &SearchManager::onSearchAnimeFinished);
     connect(this->anilist_api_, &AnilistApi::searchAnimeFailed, this, &SearchManager::onSearchAnimeFailed);
+
+    connect(this->anilist_api_, &AnilistApi::searchAnimeFinished, this, &SearchManager::onSearchAnimeFinished);
 }
 
 void SearchManager::requestAnimeSearch(const QString &title) {
-    this->startSearch(title);
-}
-
-void SearchManager::startSearch(const QString &title) {
     if (this->search_in_progress_) {
         return;
     }
 
     this->search_in_progress_ = true;
+    this->search_type_ = SearchType::Normal;
 
     emit searchProgressStarted(
         QStringLiteral("Search"),
@@ -30,18 +28,79 @@ void SearchManager::startSearch(const QString &title) {
     this->anilist_api_->searchAnime(title);
 }
 
-void SearchManager::onSearchAnimeFailed(const QString &message) {
+void SearchManager::requestQuietAnimeSearch(const QString &title) {
+    if (this->search_in_progress_) {
+        return;
+    }
+
+    this->search_in_progress_ = true;
+    this->search_type_ = SearchType::Quiet;
+
+    this->anilist_api_->searchAnime(title);
+}
+
+void SearchManager::requestAnimeSearchById(const QList<int> &media_ids) {
+    if (this->search_in_progress_) {
+        return;
+    }
+
+    this->search_in_progress_ = true;
+    this->search_type_ = SearchType::Id;
+
+    this->anilist_api_->searchAnimeIds(media_ids);
+}
+
+void SearchManager::resetSearch() {
     this->search_in_progress_ = false;
-    emit searchProgressFinished();
-    emit searchFailed(
-        QStringLiteral("Search"),
-        message
-    );
+    this->search_type_ = SearchType::None;
+}
+
+void SearchManager::onSearchAnimeFailed(const QString &message) {
+    const SearchType search_type = this->search_type_;
+    this->resetSearch();
+
+    switch (search_type) {
+        case SearchType::None:
+            break;
+        case SearchType::Normal: {
+            emit searchProgressFinished();
+            emit searchFailed(
+                QStringLiteral("Search"),
+                message
+            );
+            break;
+        }
+        case SearchType::Quiet: {
+            emit quietSearchFailed(message);
+            break;
+        }
+        case SearchType::Id: {
+            emit idSearchFailed(message);
+            break;
+        }
+    }
 }
 
 void SearchManager::onSearchAnimeFinished(const QList<AnilistMedia> &media_list) {
-    this->search_in_progress_ = false;
-    emit searchProgressFinished();
-    emit searchFinished(media_list);
+    const SearchType search_type = this->search_type_;
+    this->resetSearch();
+
+    switch (search_type) {
+        case SearchType::None:
+            break;
+        case SearchType::Normal: {
+            emit searchProgressFinished();
+            emit searchFinished(media_list);
+            break;
+        }
+        case SearchType::Quiet: {
+            emit quietSearchFinished(media_list);
+            break;
+        }
+        case SearchType::Id: {
+            emit idSearchFinished(media_list);
+            break;
+        }
+    }
 }
 
--- src/base/search/search_manager.hpp
+++ src/base/search/search_manager.hpp
@@ -16,26 +16,38 @@ public:
 
 public slots:
     void requestAnimeSearch(const QString &title);
+    void requestQuietAnimeSearch(const QString &title);
+    void requestAnimeSearchById(const QList<int> &media_ids);
 
 signals:
     void searchProgressStarted(const QString &title, const QString &message);
     void searchProgressFinished();
 
     void searchFinished(const QList<AnilistMedia> &media_list);
+    void quietSearchFinished(const QList<AnilistMedia> &media_list);
+    void idSearchFinished(const QList<AnilistMedia> &media_list);
 
     void searchFailed(const QString &title, const QString &message);
+    void quietSearchFailed(const QString &message);
+    void idSearchFailed(const QString &message);
 
 private:
+    enum class SearchType {
+        None,
+        Normal,
+        Quiet,
+        Id
+    };
     AnilistApi *anilist_api_;
 
     bool search_in_progress_ = false;
+    SearchType search_type_ = SearchType::None;
 
-    void startSearch(const QString &title);
-    void finishSearch();
+    void resetSearch();
 
 private slots:
     void onSearchAnimeFailed(const QString &message);
-    void onSearchAnimeFinished(const QList<AnilistMedia> &media_list);
 
+    void onSearchAnimeFinished(const QList<AnilistMedia> &media_list);
 };
 
