03e80cc377882f46820c93bc65e4e1df6d55cc75
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-09-16 23:25:12 +0300

    fix: append season number if any to parsed playing title

--- src/base/recognition/anime_file_parser.cpp
+++ src/base/recognition/anime_file_parser.cpp
@@ -10,10 +10,11 @@
 
 AnimeFileParser::AnimeFileInfo AnimeFileParser::parse(const QString &filename) {
     const std::string utf8_filename = filename.normalized(QString::NormalizationForm_KC).toUtf8().toStdString();
-    const std::vector<anitomy::Element> elements = anitomy::parse(utf8_filename);
+    std::vector<anitomy::Element> elements = anitomy::parse(utf8_filename);
 
     AnimeFileParser::AnimeFileInfo parsed_anime_file;
 
+    // Get the parse anime title
     const auto title_element = std::ranges::find_if(elements, [] (const anitomy::Element &element) {
         return element.kind == anitomy::ElementKind::Title;
     });
@@ -21,6 +22,7 @@ AnimeFileParser::AnimeFileInfo AnimeFileParser::parse(const QString &filename) {
         parsed_anime_file.title = QString::fromStdString(title_element->value);
     }
 
+    // Get the parse episode number
     const auto episode_element = std::ranges::find_if(elements, [] (const anitomy::Element &element) {
         return element.kind == anitomy::ElementKind::Episode;
     });
@@ -30,6 +32,24 @@ AnimeFileParser::AnimeFileInfo AnimeFileParser::parse(const QString &filename) {
             episode_element->value.data() + episode_element->value.size(),
             parsed_anime_file.episode,
             10
+        );
+    }
+
+    // Get the parsed season text and append it to the title
+    const auto season_element = std::ranges::find_if(elements, [] (const anitomy::Element &element) {
+        return element.kind == anitomy::ElementKind::Season;
+    });
+    if (season_element != elements.end()) {
+        const auto first_non_zero = season_element->value.find_first_not_of('0');
+
+        if (first_non_zero == std::string::npos) {
+            season_element->value = "0";
+        } else {
+            season_element->value.erase(0, first_non_zero);
+        }
+
+        parsed_anime_file.title.append(
+            QStringLiteral(" S%1").arg(season_element->value)
         );
     }
 
